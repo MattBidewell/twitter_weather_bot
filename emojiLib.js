e220449f60f0d43eb56e2465446a87deb53442c5
Matthew <matthewbidewell@gmail.com>
2017-09-30 21:01:45 +0100

    Libary of emojis

---
+++ emojiLib.js
@@ -0,0 +1,66 @@
+module.exports = {
+    "sunny":{
+        "char":"☀️",
+        "uniCode":'\u2600'             
+    },
+    "cloudy":{
+        "char":"☁️",
+        "uniCode":'\u2601'   
+    },
+    "partly sunny":{
+        "char": "⛅",
+        "uniCode":'\u26c5'           
+    },
+    "mostly sunny":{
+        "char": "⛅",        
+        "uniCode":'\u26c5'           
+    },
+    "mostly clear":{
+        "char": "⛅",        
+        "uniCode":'\u26c5'           
+    },
+    "haze":{
+        "char": "🌫",
+        "uniCode":'\u1f32b'
+    },
+    "clear":{
+        "char": "☀️",
+        "uniCode":'\u2600'                     
+    },
+    "rain showers":{
+        "char": "🌧",
+        'uniCode':'\u1F327'
+    },
+    "rain":{
+        "char": "🌧",
+        'uniCode':'\u1F327'        
+    },
+    "showers":{
+        "char": "🌦",
+        'uniCode':'\u1F326'    
+    },
+    "shower":{
+        "char": "🌦" ,
+        'uniCode':'\u1F326'            
+    },
+    "fair":{
+        "char": "⛅",
+        "uniCode":'\u26c5'                   
+    },
+    "snow":{
+        "char": "❄️",
+        "uniCode":'\u2744'
+    },
+    "light rain":{
+        "char": "🌦",
+        'uniCode':'\u1F326'                    
+    },
+    "t-storms":{
+        "char": "⛈",
+        'uniCode':'\u26C8'
+    },
+    "storms":{
+        "char": "⛈",
+        'uniCode':'\u26C8'        
+    }
+};
