356c2e215d94a633f0a492e5972c251a9c194257
Matthew <matthewbidewell@gmail.com>
2017-10-04 21:20:59 +0100

    Tidied up code

--- emojiLib.js
+++ emojiLib.js
@@ -1,70 +1,72 @@
 module.exports = {
-    "sunny":{
-        "char":"☀️",
-        "uniCode":'\u2600'             
-    },
-    "cloudy":{
-        "char":"☁️",
-        "uniCode":'\u2601'   
-    },
-    "mostly cloudy":{
-        "char":"☁️",
-        "uniCode":"\u2601"   
-    },
-    "partly sunny":{
-        "char": "⛅",
-        "uniCode":'\u26c5'           
-    },
-    "mostly sunny":{
-        "char": "⛅",        
-        "uniCode":'\u26c5'           
-    },
-    "mostly clear":{
-        "char": "⛅",        
-        "uniCode":'\u26c5'           
-    },
-    "haze":{
-        "char": "🌫",
-        "uniCode":'\u1f32b'
-    },
-    "clear":{
-        "char": "☀️",
-        "uniCode":'\u2600'                     
-    },
-    "rain showers":{
-        "char": "🌧",
-        'uniCode':'\u1F327'
-    },
-    "rain":{
-        "char": "🌧",
-        'uniCode':'\u1F327'        
-    },
-    "showers":{
-        "char": "🌦",
-        'uniCode':'\u1F326'    
-    },
-    "shower":{
-        "char": "🌦" ,
-        'uniCode':'\u1F326'            
-    },
-    "fair":{
-        "char": "⛅",
-        "uniCode":'\u26c5'                   
-    },
-    "snow":{
-        "char": "❄️",
-        "uniCode":'\u2744'
-    },
-    "light rain":{
-        "char": "🌦",
-        'uniCode':'\u1F326'                    
-    },
-    "t-storms":{
-        "char": "⛈",
-        'uniCode':'\u26C8'
-    },
-    "storms":{
-        "char": "⛈",
-        'uniCode':'\u26C8'        
+    "emojis":{
+        "sunny":{
+            "char":"☀️",
+            "uniCode":'\u2600'             
+        },
+        "cloudy":{
+            "char":"☁️",
+            "uniCode":'\u2601'   
+        },
+        "mostly_cloudy":{
+            "char":"☁️",
+            "uniCode":"\u2601"
+        },
+        "partly_sunny":{
+            "char": "⛅",
+            "uniCode":'\u26c5'           
+        },
+        "mostly_sunny":{
+            "char": "⛅",        
+            "uniCode":'\u26c5'           
+        },
+        "mostly_clear":{
+            "char": "⛅",        
+            "uniCode":'\u26c5'           
+        },
+        "haze":{
+            "char": "🌫",
+            "uniCode":'\u1f32b'
+        },
+        "clear":{
+            "char": "☀️",
+            "uniCode":'\u2600'                     
+        },
+        "rain_showers":{
+            "char": "🌧",
+            'uniCode':'\u1F327'
+        },
+        "rain":{
+            "char": "🌧",
+            'uniCode':'\u1F327'        
+        },
+        "showers":{
+            "char": "🌦",
+            'uniCode':'\u1F326'    
+        },
+        "shower":{
+            "char": "🌦" ,
+            'uniCode':'\u1F326'            
+        },
+        "fair":{
+            "char": "⛅",
+            "uniCode":'\u26c5'                   
+        },
+        "snow":{
+            "char": "❄️",
+            "uniCode":'\u2744'
+        },
+        "light_rain":{
+            "char": "🌦",
+            'uniCode':'\u1F326'                    
+        },
+        "t-storms":{
+            "char": "⛈",
+            'uniCode':'\u26C8'
+        },
+        "storms":{
+            "char": "⛈",
+            'uniCode':'\u26C8'        
+        }
     }
 };
