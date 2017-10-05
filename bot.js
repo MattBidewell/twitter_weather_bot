359fa725de088d66c0f71612be48c4b6881f435c
Matthew <matthewbidewell@gmail.com>
2017-10-05 14:11:43 +0100

    Tidied up code and added comments.

--- bot.js
+++ bot.js
@@ -1,37 +1,37 @@
 console.log("Bot active");
 
-const weather = require('weather-js');
-const twit = require('twit');
+const weather = require('weather-js'); //require weather api
+const twit = require('twit'); //require twitter api
 const config = require('./config');
 
-let emojisFile = require('./emojiLib');
-const emojiLib = emojisFile.emojis;
-
+let emojisFile = require('./emojiLib'); //retrieve content from emojiLib file
+const emojiLib = emojisFile.emojis; //retrieve emojis
 
 var t = new twit(config.twitterKey);//weather robot twitter obj
 
-var host = "TheWeatherRobo";
-var reply = "";
-//watch for tweets mentioning host name.
+var host = "TheWeatherRobo"; //Twitter @ name.
+
+//initiate watch for tweets mentioning host name.
 var stream = t.stream('statuses/filter', { track: host });
 
-//Take tweet.
+//When tweet mentions host.
 stream.on('tweet', function (tweet) {
-  var sender = tweet.user.screen_name; //sender name
-  var txt = tweet.text; //txt
-  var regex = /@TheWeatherRobo /i; //regex expression
-  var loc = txt.replace(regex, ''); //Gets location from tweet text.
+
+  var txt = tweet.text; //tweet text
+  var regex = /@TheWeatherRobo /i; //regex remove host name from tweet
+  var loc = txt.replace(regex, ''); //removes host name from tweet to leave with location
+  var reply; //initate reply
+  var nameID = tweet.id_str; //get the twitter ID of sender
+  var name = tweet.user.screen_name; //gets the twitter screen name of sender
 
   /*
-    Uses location from tweet to get weather from API.
+    Uses location from tweet to get weather from API. Automatically sets to celcius since
+    celcius is life.
   */
   weather.find({search: loc, degreeType: 'C'}, function(err, result){
     if(err)throw err;
-    var reply;
-    var nameID = tweet.id_str;
-    var name = tweet.user.screen_name;
 
-    if(result[0] == undefined){
+    if(result[0] == undefined){ //if location does not exist or MSN weather cannot find it
       reply = "Unfortunetly, I can't retrieve that location";
       replyTweet(reply);
     }
@@ -39,33 +39,35 @@ stream.on('tweet', function (tweet) {
     else{
       //Takes important info and creates reply
       
-      let weather = result[0].current.skytext;
-      let weatherEmoji;
-      let weatherRegexed = weather.replace(/ /g,"_");
-       
+      let weather = result[0].current.skytext; //weather
+      let weatherEmoji = "⛅"; //emoji var
+      let weatherRegexed = weather.replace(/ /g,"_"); //removes spaces and replaces with _
+      
+      //searched through emojiLib
       for(emojiName in emojiLib){
+        //comparest the name of the emoji in emojiLib with the weather
         if(emojiName === weatherRegexed.toLowerCase()){
+            //if match then emoji becomes part of the reply.
             weatherEmoji = emojiLib[emojiName].char;
-            //weather = emojiLib.weatherRegexed.char;
             break;
         }
       }
 
       var degrees = String.fromCharCode(176) + "C"; // degress and celcius
+
+      //template literal/lateral string
       reply = 
       `🌎:${result[0].location.name}\n🌡️:${result[0].current.temperature}${degrees}\n${weatherEmoji}:${weather}`;
       replyTweet(reply);
     }
 
-    console.log(nameID +  " @" + name + "\n" + reply + "\n");
-
     function replyTweet(reply){
-      //Creates the response tweet JSON.
+      //Creates the response tweet JSON. 
       var res = {
-        status: '@' + tweet.user.screen_name + " " + reply,
-        in_reply_to_status_id: '' + tweet.id_str
+        status: `@${name} ${reply}`, //template lateral string
+        in_reply_to_status_id: nameID
       };
-      //posts response tweetgg
+      //posts response tweet
       t.post('statuses/update',res);
     }
   });
