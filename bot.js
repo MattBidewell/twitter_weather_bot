97f02ff03068c448068d83617936438307ce9347
Matthew <matthewbidewell@gmail.com>
2018-01-25 13:39:23 +0000

    removed unneeded code

--- bot.js
+++ bot.js
@@ -114,9 +114,3 @@ stream.on('tweet', function(tweet) {
     }
   });
 });
-
-process.on('uncaughtException', function(err) {
-  console.error(`${(new Date).toUTCString()} uncaughtException:`, err.message)
-  console.error(err.stack)
-  process.exit(1)
-})
